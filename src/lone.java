89449055b330b1b37a8b4a8723971014eed99d7c
N0ct1s <n0ct1s-prgrmmr@pm.me>
2023-09-27 14:05:20 +0100

    exe 2

---
+++ src/lone.java
@@ -0,0 +1,23 @@
+//import java.util.Scanner;
+//
+//public class lone {
+//    static float calculate_load(float amount, float rate, int years) {
+//        float payment
+//    }
+//    public static void main(String[] args) {
+//        static float calculate_loan(float amount, float rate, int years) {
+//            float payment = amount * ( rate / ( 1 - (1 + rate)));
+//        }
+//
+//        Scanner u_input = new Scanner(System.in);
+//
+//        System.out.print("Loan amount: £");
+//        float loan_amount = u_input.nextFloat();
+//
+//        System.out.print("Interest Rate (APR %): ");
+//        float rate = u_input.nextFloat();
+//
+//        System.out.print("Number of years: ");
+//        int num_years = u_input.nextInt();
+//    }
+//}
